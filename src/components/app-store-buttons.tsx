20f68d9c67223039b03d2a815f613e93eee3d0a4
SmartSapp Info <info@smartsapp.com>
2026-01-22 15:28:15 +0000

    lET'S Update apple logo for download:

--- src/components/app-store-buttons.tsx
+++ src/components/app-store-buttons.tsx
@@ -13,7 +13,7 @@ const AppStoreButtons = () => {
         <Image src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png" alt="Get it on Google Play" width={300} height={89} className="h-12 w-auto" />
       </a>
       <a href={storeLinks.ios} target="_blank" rel="noopener noreferrer" className="transition-transform hover:scale-105">
-        <Image src="https://smartsapp.com/wp-content/uploads/2021/04/apple-store-badge.png" alt="Download on the App Store" width={162} height={48} className="h-12 w-auto" />
+        <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Download_on_the_App_Store_Badge.svg/1280px-Download_on_the_App_Store_Badge.svg.png" alt="Download on the App Store" width={1280} height={430} className="h-12 w-auto" />
       </a>
       <a href={storeLinks.huawei} target="_blank" rel="noopener noreferrer" className="transition-transform hover:scale-105">
         <Image src="https://smartsapp.com/wp-content/uploads/2021/04/huawei-app-gallery.png" alt="Explore it on AppGallery" width={162} height={48} className="h-12 w-auto" />
