1527cf0e4e1008cd3724d102a0d54e9d8a0c4a6e
SmartSapp Info <info@smartsapp.com>
2026-01-28 07:16:04 +0000

    let's make the how to videos section only two columns, instead of three.

--- src/components/school-meeting-loader.tsx
+++ src/components/school-meeting-loader.tsx
@@ -273,7 +273,7 @@ export default function SchoolMeetingLoader({ slug }: SchoolMeetingLoaderProps) 
                         Useful Help Videos
                     </h2>
 
-                    <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+                    <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
                         {helpVideos.map((url, index) => (
                         <VideoEmbed key={index} url={url} />
                         ))}
