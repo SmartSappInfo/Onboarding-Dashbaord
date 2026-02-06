5c6f40371f9f7473b3257cb5df1e497583c9b400
SmartSapp Info <info@smartsapp.com>
2026-02-06 07:31:39 +0000

    let's make the navbar fully rounded and shorter. let it span 70% of the

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <header className="fixed top-0 z-50 w-full py-4">
       <div className="container">
-        <div className="flex items-center justify-between rounded-lg bg-black/20 backdrop-blur-sm border border-white/10 py-2 px-4">
+        <div className="flex items-center justify-between rounded-full bg-black/20 backdrop-blur-sm border border-white/10 py-1 px-4">
           <Link href="/" aria-label="Back to homepage">
             <Logo className="h-8" variant="white" />
           </Link>
