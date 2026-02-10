9df5114277ac0cb83c7818fa02a275373ad7049f
SmartSapp Info <info@smartsapp.com>
2026-02-10 06:27:58 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://logo.cl

--- next.config.ts
+++ next.config.ts
@@ -31,6 +31,12 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'logo.clearbit.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'smartsapp.com',
         port: '',
         pathname: '/**',
