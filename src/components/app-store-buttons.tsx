746a6f22366b37673282f5242b34f19f5e4532fb
SmartSapp Info <info@smartsapp.com>
2026-02-17 18:38:04 +0000

    Apple download logo: https://firebasestorage.googleapis.com/v0/b/studio-

--- src/components/app-store-buttons.tsx
+++ src/components/app-store-buttons.tsx
@@ -11,29 +11,29 @@ const AppStoreButtons = () => {
     <div className="flex flex-wrap items-center justify-center gap-6">
       <a href={storeLinks.android} target="_blank" rel="noopener noreferrer" className="relative block h-14 w-44 transition-transform hover:scale-105">
         <Image 
-          src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png" 
+          src="https://firebasestorage.googleapis.com/v0/b/studio-9220106300-f74cb.firebasestorage.app/o/Logo_PlayStore%20(1).png?alt=media&token=9b67f0c9-3030-4de1-ba17-b07dc799846c" 
           alt="Get it on Google Play" 
           fill 
           sizes="176px"
-          className="object-cover"
+          className="object-contain"
         />
       </a>
       <a href={storeLinks.ios} target="_blank" rel="noopener noreferrer" className="relative block h-14 w-44 transition-transform hover:scale-105">
         <Image 
-          src="https://upload.wikimedia.org/wikipedia/commons/3/3c/Download_on_the_App_Store_Badge.svg" 
+          src="https://firebasestorage.googleapis.com/v0/b/studio-9220106300-f74cb.firebasestorage.app/o/Logo_AppleStore.png?alt=media&token=e7c54394-916b-482c-88ef-d3b051fe847c" 
           alt="Download on the App Store" 
           fill
           sizes="176px"
-          className="object-cover"
+          className="object-contain"
         />
       </a>
       <a href={storeLinks.huawei} target="_blank" rel="noopener noreferrer" className="relative block h-14 w-44 transition-transform hover:scale-105">
         <Image 
-          src="https://smartsapp.com/img/download-buttons/huawei-app-gallery.png" 
+          src="https://firebasestorage.googleapis.com/v0/b/studio-9220106300-f74cb.firebasestorage.app/o/Logo_HuaweiStore.png?alt=media&token=99bf0821-d46d-456c-a867-285c2f161eb0" 
           alt="Explore it on AppGallery" 
           fill
           sizes="176px"
-          className="object-cover"
+          className="object-contain"
         />
       </a>
     </div>
